c2ac40223d87c70fa56e404cfef1389d3e617586
lautarofrioni <lauti.tatengue@hotmail.com>
2024-11-05 15:50:57 -0300

    listo

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
 
-
 //ALGORITMO CON CICLOS
 
-//const nombre = prompt("ingrese el nombre del estudiante");
+/**
+ * Este programa consiste en que el estudiante ingrese sus datos (Nombre y apellido) y por ultimo la sección (aula) donde estudia.
+ */
 
-//const apellido = prompt("ingrese el apellido del estudiante");
+//const nombre = prompt("Ingrese el nombre del estudiante");
 
-//const sección = prompt("ingrese la sección del aula");
+//const apellido = prompt("Ingrese el apellido del estudiante");
 
+//let sección = parseInt(prompt("Ingrese la sección donde estudia"));
 
-
-//while (nombre) || (apellido) || (sección) {
-//    if(nombre);
+//while (isNaN(sección)){
     
-//    nombre = prompt("ingrese el nombre del estudiante");
-
-
-
+//    alert("Sección incorrecta");
+    
+//    sección = parseInt(prompt("Por favor, ingrese la sección correcta donde estudia"))
 //}
 
+//alert ("Bienvenido estudiante " + nombre + " " + apellido);
 
 
 
 
-// ALGORITMO CON CONDICIONES
 
 
-//let numero1 = parseInt(prompt("ingrese su primera nota"));
 
-//let numero2 = parseInt(prompt("ingrese su segunda nota"));
 
-//let numero3 = parseInt(prompt("ingrese su tercera nota"));
+//ALGORITMO CON CONDICIONES
+
+/**
+ * Este programa consiste en calcular el promedio con las 3 notas del estudiante para comprobar si esta aprobado o desaprobado
+ */
+
+
+//let numero1 = parseInt(prompt("Ingrese su primera nota"));
+
+//let numero2 = parseInt(prompt("Ingrese su segunda nota"));
+
+//let numero3 = parseInt(prompt("Ingrese su tercera nota"));
 
 //let resultado = (numero1 + numero2 + numero3) / 3;
 
@@ -46,9 +54,6 @@
 
 
 //console.log(resultado);
-
-
-
 
 
 
